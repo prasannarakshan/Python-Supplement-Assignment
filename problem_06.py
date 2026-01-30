f658cb500ae69aa73e40284db82efda3d899f7e8
prasannarakshan <prasannarakshan6@gmail.com>
2026-01-30 15:05:23 +0530

    for i in range(len(text) -1 , -1 , -1):

--- problem_06.py
+++ problem_06.py
@@ -1,8 +1,7 @@
 # Problem 6: Reverse a string
 # Find and fix the error
-
 text = "Python"
 reversed_text = ""
-for i in range(len(text), 0, -1):
+for i in range(len(text) - 1, -1, -1):  # start at last index
     reversed_text += text[i]
 print(f"Reversed: {reversed_text}")
